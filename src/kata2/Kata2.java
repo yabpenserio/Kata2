1e1869329ac769d3d9bed3f78ad0182c8cba5dd0
yabpe <yabpe@10.193.29.184>
2022-09-29 12:22:02 +0100

    Segundo Commit

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -13,18 +13,15 @@ public class Kata2 {
        
         
         for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])){
-                histogram.put(data[i], histogram.get(data[i])+1);
-            }else{
-                histogram.put(data[i], 1);
-            }
+            histogram.put(data[i],histogram.containsKey(data[i])? 
+                            histogram.get(data[i])+1:1);
             
         }
         for (Map.Entry<Integer,Integer> entry : histogram.entrySet()) {
             System.out.println(entry.getKey() + " ==> " + entry.getValue());
 
         }
-        System.out.println("");
+        
     }
     
 }
